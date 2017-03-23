e1649c76f638a680524839c77d260aa2ed01dac4
Peter Große <pegro@friiks.de>
2017-03-23 01:07:44 +0100

    DBConnection: fix exception handling

--- source/model/DBConnection.php
+++ source/model/DBConnection.php
@@ -66,27 +66,29 @@ class DBConnection extends \PDO
     {
         static $time_last_use;
         static $instance;
-        if (!isset($instance)) {
-            static $connection_try;
-            if (!$connection_try) {
-                $instance = new self();
-                $connection_try = true;
+        try {
+            if (!isset($instance)) {
+                static $connection_try;
+                if (!$connection_try) {
+                    $instance = new self();
+                    $connection_try = true;
+                    $time_last_use = time();
+                }
+            } else {
+                // if database handle wasn't used for some time, check if connection is still alive
+                if($time_last_use < time() - self::$defaultConfig['check_alive_time']) {
+                    try {
+                        $instance->query('SELECT 1');
+                    } catch(\PDOException $e) {
+                        Logger::getInstance()->info('Database connection got lost. Try reconnecting...');
+
+                        $instance = new self();
+                    }
+                }
                 $time_last_use = time();
             }
-        } else {
-            // if database handle wasn't used for some time, check if connection is still alive
-            if($time_last_use < time() - self::$defaultConfig['check_alive_time']) {
-                try {
-                    $instance->query('SELECT 1');
-                } catch(\PDOException $e) {
-                    Logger::getInstance()->info('Database connection got lost. Try reconnecting...');
-
-                    $instance = new self();
-                }
-            }
-            $time_last_use = time();
-        }
-        if(!$instance) {
+        } catch (\Exception $e) {
+            Logger::getInstance()->exception($e);
             die(_('No Datebase connection.'));
         }
         return $instance;
@@ -113,25 +115,20 @@ class DBConnection extends \PDO
      */
     public function __construct()
     {
-        try {
-            $config = array_merge(self::$defaultConfig, Config::get('database'));
+        $config = array_merge(self::$defaultConfig, Config::get('database'));
 
-            parent::__construct(
-                'pgsql:host='.$config['host'].';'
-                .'dbname='.$config['name'].';'
-                .'port='.$config['port'],
-                $config['user'],
-                $config['pass']
-            );
-#            Config::unsetForDatabase();
+        parent::__construct(
+            'pgsql:host='.$config['host'].';'
+            .'dbname='.$config['name'].';'
+            .'port='.$config['port'],
+            $config['user'],
+            $config['pass']
+        );
 
-            $this->setAttribute(\PDO::ATTR_STATEMENT_CLASS, [__NAMESPACE__.'\\DBStatement', [$this]]);
-            $this->setAttribute(\PDO::ATTR_ERRMODE, \PDO::ERRMODE_EXCEPTION);
-            $this->exec("SET CLIENT_ENCODING TO 'UTF8'");
-            $this->exec("SET search_path TO ".$config['namespace'].";");
-        } catch (\PDOException $e) {
-            Logger::getInstance()->exception($e);
-        }
+        $this->setAttribute(\PDO::ATTR_STATEMENT_CLASS, [__NAMESPACE__.'\\DBStatement', [$this]]);
+        $this->setAttribute(\PDO::ATTR_ERRMODE, \PDO::ERRMODE_EXCEPTION);
+        $this->exec("SET CLIENT_ENCODING TO 'UTF8'");
+        $this->exec("SET search_path TO ".$config['namespace'].";");
     } // constructor
 
 
